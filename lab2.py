f22ba6efda3c5d1d6c8dd436feb57069680a1189
Victor LaBrie <vplabrie@gmail.com>
2022-11-20 05:12:29 -0600

    Program written, need to try on a fresh python installation

--- lab2.py
+++ lab2.py
@@ -7,24 +7,73 @@ import numpy as np
 
 
 class SimulatedAnnealing:
-    def __init__(self) -> None:
-        print('init called')
-    def __call__(prob) -> Any:
-        pass
+    def __init__(self, budget):
+        self.budget = budget
+        self.algorithm_id = np.random.randint(100)
+
+    def __call__(self, func: problem.PBO):
+        func.state.evaluations
+        self.f_opt = 0
+        self.x_opt = None
+
+        #variables:
+
+        initialTemp = 1
+        #exponential: alpha between 0 and 1
+        #linear and quadratic: alpha above 0
+        alpha = 0.5
+        
+        for i in range(self.budget):
+            n = func.meta_data.n_variables
+            x = np.random.choice([0,1],n)
+            #COOLING SCHEDULES
+            #Exponential multicplicative:
+            temperature = initialTemp * pow(alpha, i)
+            
+            #Linear multiplicative:
+            #temperature = initialTemp/(1+(alpha*i))
+            
+            #Quadratic multiplicative:
+            #temperature = initialTemp/(a*pow(i,2))
+
+            f = func(x)
+
+            #deltaE
+            deltaE = f-self.f_opt
+            
+            #If better, accept
+            if deltaE > 0:
+                self.f_opt = f
+                self.x_opt = x
+            
+            #If not better but probability given temperature allows for
+            else:
+                tempProb = np.exp(deltaE*temperature)
+                if np.random.choice([0,1],[(1-tempProb),tempProb])==1:
+                    self.f_opt = f
+                    self.x_opt = x
+
+
+
+        return self.f_opt, self.x_opt
+
+    def reset(self):
+        self.algorithm_id = np.random.randint(100)
+
 
 #expermient setup for my algo     
 exp = Experiment(
-    SimulatedAnnealing(),   # instance of optimization algorithm
-    [1,2,23],                    # list of problem id's
-    [1,1,1],                 # list of problem instances
-    [4,9,16,25,36,49,64,81,100],                    # list of problem dimensions
+    SimulatedAnnealing(10),   # instance of optimization algorithm
+    [23],                    # list of problem id's
+    [1],                 # list of problem instances
+    [4],                    # list of problem dimensions
     problem_type = 'PBO',  # the problem type, function ids should correspond to problems of this type
     njobs = 2,              # the number of parrellel jobs for running this experiment
     reps = 10,      
     logged=True,         
     folder_name="IOH_Data",
     algorithm_name="SimulatedAnnealing",
-    logged_attributes = ['seed'],
+    #logged_attributes = ['seed'],
     merge_output=True,
     zip_output=True,
     remove_data=True
